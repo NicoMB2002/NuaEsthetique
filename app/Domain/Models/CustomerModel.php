617aca4aa625729ded8a99c2a2c0c27d4a72ff8b
Luke Ryan Nwantoly <2374472@edu.vaniercollege.qc.ca>
2025-10-09 10:21:50 -0400

    changed sql file name and added Create, Read, Delete functions to models

--- app/Domain/Models/CustomerModel.php
+++ app/Domain/Models/CustomerModel.php
@@ -10,4 +10,49 @@ class CustomerModel extends BaseModel
         parent::__construct($pdo_service);
     }
 
+    public function getCustomers(){
+        $query = "Select * from Customers";
+        $customers = $this->selectAll($query);
+        return $customers;
+    }
+
+    public function getOneCustomer($id){
+        $query = "Select * from Customers where customer_id = :customer_id";
+        $customer = $this->selectOne($query,['customer_id'=>$id]);
+        return $customer;
+    }
+
+    public function insertCustomer($info = []){
+    $query = "INSERT INTO `customers`(
+            first_name,
+            last_name,
+            phone,
+            address,
+            postal_code,
+            date_of_birth,
+            receive_notification,
+            email
+        )
+        VALUES(
+            :first_name,
+            :last_name,
+            :phone,
+            :address,
+            :postal_code,
+            :date_of_birth,
+            :receive_notification,
+            :email
+        )";
+
+        $this->execute($query, $info);
+
+    }
+
+    public function deleteCustomer($id){
+        $query = "Delete from Customers where customer_id = :customer_id";
+
+        $this->execute($query,['customer_id'=>$id]);
+    }
+
+
 }
--- app/Domain/Models/EmployeeModel.php
+++ app/Domain/Models/EmployeeModel.php
@@ -10,4 +10,52 @@ class EmployeeModel extends BaseModel
         parent::__construct($pdo_service);
     }
 
+
+
+    public function getEmployees(){
+        $query = "Select * from Employees";
+        $employees = $this->selectAll($query);
+        return $employees;
+    }
+
+    public function getOneEmployee($id){
+        $query = "Select * from Employees where employee_id = :employee_id";
+        $employee = $this->selectOne($query,['employee_id'=>$id]);
+        return $employee;
+    }
+
+    public function insertEmployee($info = []){
+    $query = "INSERT INTO employees(
+                first_name,
+                last_name,
+                hire_date,
+                date_of_birth,
+                phone,
+                email,
+                address,
+                postal_code,
+                isAdmin
+            )
+        VALUES(
+                :first_name,
+                :last_name,
+                :hire_date,
+                :date_of_birth,
+                :phone,
+                :email,
+                :address,
+                :postal_code,
+                :isAdmin
+        )";
+
+        $this->execute($query, $info);
+
+    }
+
+    public function deleteEmployee($id){
+        $query = "Delete from Employees where employee_id = :employee_id";
+
+        $this->execute($query,['employee_id'=>$id]);
+    }
+
 }
